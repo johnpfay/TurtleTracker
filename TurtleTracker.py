1586fa7b09c173725b3fd63f1c217a2d73ac67fc
johnpfay <john.fay@duke.edu>
2025-09-29 11:56:38 -0400

    Revert "Change to while loop"
    This reverts commit d74da1202fcf3cbb0b074749814efa9f82361a26.

--- TurtleTracker.py
+++ TurtleTracker.py
@@ -8,7 +8,6 @@
 # Author: John Fay (john.fay@duke.edu)
 # Date:   Fall 2025
 #--------------------------------------------------------------
-
 #Create a variable pointing to the data file
 file_name = './data/raw/sara.txt'
 
@@ -16,13 +15,15 @@ file_name = './data/raw/sara.txt'
 file_object = open(file_name,'r')
 
 #Read contents of file into a list
-lineString = file_object.readline()
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
 
 #Pretend we read one line of data from the file
-while lineString:
+for lineString in line_list:
     # Check if line is a data line
     if lineString[0] in ("#","u"):
-        lineString = file_object.readline()
         continue
 
     #Split the string into a list of data items
@@ -37,9 +38,3 @@ while lineString:
 
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-
-    #Move to the next line
-    lineString = file_object.readline()
-
-# Close the file
-file_object.close()
