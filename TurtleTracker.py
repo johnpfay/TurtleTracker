88a8b9f01f2d9039cbf6297dc79c49a225dd8cc3
johnpfay <john.fay@duke.edu>
2025-10-01 08:14:03 -0400

    Insert line to ask user for the date

--- TurtleTracker.py
+++ TurtleTracker.py
@@ -8,6 +8,10 @@
 # Author: John Fay (john.fay@duke.edu)
 # Date:   Fall 2025
 #--------------------------------------------------------------
+
+# Ask the user for a date, specifying the format
+user_date = input("Enter a date (M/D/YYYY):")
+
 #Create a variable pointing to the data file
 file_name = './data/raw/sara.txt'
 
